678766115008e4828f25428de2b09d69fba5dbac
mikohan <angara99@gmail.com>
2021-02-28 17:59:46 +0300

    Schema working

---
+++ books/schema.py
@@ -0,0 +1,9 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import Books
+
+
+class BooksType(DjangoObjectType):
+    class Meta:
+        model = Books
+        fields = ("id", "title", "excerpt")
