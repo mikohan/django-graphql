9bc9a49f07b8e2f1aed48a5a69e9215f4d43f5d7
mikohan <angara99@gmail.com>
2021-02-28 17:41:37 +0300

    Made book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class Books(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 # Create your models here.
