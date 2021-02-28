eda8ec4a7ac289cc5291a0b362b7a4e531ff49e8
mikohan <angara99@gmail.com>
2021-02-28 18:05:36 +0300

    Added shema

--- books/schema.py
+++ books/schema.py
@@ -7,3 +7,10 @@ class BooksType(DjangoObjectType):
     class Meta:
         model = Books
         fields = ("id", "title", "excerpt")
+
+
+class Query(graphene.ObjectType):
+    ass_books = graphene.List(BooksType)
+
+
+schema = graphene.Schema(query=Query)
