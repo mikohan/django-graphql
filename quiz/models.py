794ddf7ab9fa14cb2d531e1009edb25791803335
mikohan <angara99@gmail.com>
2021-03-01 09:09:32 +0300

    Added models

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Quezzes(models.Model):
+    title = models.CharField(max_length=255, default=_("New Quiz"))
+    category = models.ForeignKey(Category, default=1, on_delete=models.DO_NOTHING)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+
+    SCALE = (
+        (0, _("Fundamental")),
+        (1, _("Begginer")),
+        (2, _("Intermediate")),
+        (3, _("Advanced")),
+        (4, _("Expert")),
+    )
+
+    TYPE = (0, _("Multiple Choice"))
