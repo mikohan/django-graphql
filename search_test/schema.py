b11810cfcb66b46200985c77dc4aa93283d019ee
mikohan <angara99@gmail.com>
2021-03-12 11:42:45 +0300

    Added requests

--- search_test/schema.py
+++ search_test/schema.py
@@ -1,6 +1,11 @@
 from graphene import String, ObjectType, Int, Field, Schema
 from graphene_django import DjangoObjectType
 from graphene_django import DjangoListField
+from elasticsearch_dsl import Search
+from elasticsearch import Elasticsearch
+
+# connections.create_connection(hosts=["localhost:9200"], timeout=20)
+es = Elasticsearch(["http://localhost:9200"])
 
 
 class Product(ObjectType):
@@ -13,6 +18,11 @@ class Query(ObjectType):
     product = Field(Product, id=Int())
 
     def resolve_product(root, info, id):
+
+        request = Search(using=es, index="prod_notebook")
+        response = request.source(["id", "name"])
+        for item in response:
+            print(item)
         return {"id": id, "name": "Vladimir"}
 
     elastic = String()
