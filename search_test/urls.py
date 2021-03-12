a41405b6c72de7af6a7e7cb8a4544bfa0b4e7dc2
mikohan <angara99@gmail.com>
2021-03-12 17:01:31 +0300

    Raw proxy working

--- search_test/urls.py
+++ search_test/urls.py
@@ -1,4 +1,4 @@
-from search_test.views import SearchView
+from search_test.views import SearchView, send_json
 from django.urls import path
 
 from graphene_django.views import GraphQLView
@@ -9,4 +9,5 @@ urlpatterns = [
     # Only a single URL to access GraphQL
     path("graphql", GraphQLView.as_view(graphiql=True, schema=schema)),
     path("query", SearchView.as_view(), name="search"),
+    path("jsontest", send_json, name="send_json"),
 ]
--- search_test/views.py
+++ search_test/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.base import TemplateView
-from django.http import HttpResponse
+from django.http import JsonResponse
 from .models import Post
-import json
+import json, requests
 
 
 class SearchView(TemplateView):
@@ -20,3 +20,31 @@ class SearchView(TemplateView):
 
 
 # Create your views here.
+
+
+def send_json(request):
+    query = request.GET.get("q")
+    if not query:
+        query = 1
+    data_aggs = json.dumps(
+        {
+            "size": 20,
+            "query": {"match": {"categories.cat_parent": query}},
+            "aggs": {
+                "categories": {"terms": {"field": "categories.cat_name.keyword"}},
+                "brands": {"terms": {"field": "brand.brand_name.keyword"}},
+                "engines": {"terms": {"field": "engines.engine_name.keyword"}},
+                "car_models": {"terms": {"field": "car_model.model_name.keyword"}},
+            },
+        }
+    )
+
+    r = requests.get(
+        "http://localhost:9200/prod_notebook/_search",
+        headers={"Content-Type": "application/json"},
+        data=data_aggs,
+    )
+    response = r.json()
+    data = response
+
+    return JsonResponse(data, safe=False)
