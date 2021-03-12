d39ffc9562d247a6d5a4677bbdafc304df99f67e
mikohan <angara99@gmail.com>
2021-03-12 13:34:19 +0300

    Nested objects done

--- search_test/schema.py
+++ search_test/schema.py
@@ -1,37 +1,78 @@
-from graphene import String, ObjectType, Int, Field, Schema
+from graphene import String, ObjectType, Int, ID, Field, Schema, Interface, List
 from graphene_django import DjangoObjectType
 from graphene_django import DjangoListField
 from elasticsearch_dsl import Search
 from elasticsearch import Elasticsearch
 import requests, json
 
+
 # connections.create_connection(hosts=["localhost:9200"], timeout=20)
 es = Elasticsearch(["http://localhost:9200"])
+
+
+class ICategories(Interface):
+    cat_parent = ID(required=True)
+    cat_name = String(required=True)
+    cat_parent = ID(required=False)
+
+
+class IProduct(Interface):
+    id = ID(required=True)
+    name = String(required=True)
+    categories = List(ICategories)
+
+
+class Categories(ObjectType):
+    cat_id = ID()
+    cat_name = String()
+    cat_parent = ID()
 
 
 class Product(ObjectType):
     id = Int()
     name = String()
+    categories = Field(Categories)
 
 
 class Query(ObjectType):
+    class Meta:
+        interfaces = (ICategories, IProduct)
 
-    product = Field(Product, id=Int())
+    product = List(Product)
 
-    def resolve_product(root, info, id):
-        data = json.dumps({"query": "size": 1000, {"term": {"car_model.model_name.keyword": "HD72"}}})
+    def resolve_product(root, info):
+        data = json.dumps(
+            {"size": 10, "query": {"term": {"car_model.model_name.keyword": "HD72"}}}
+        )
+        # data_aggs = json.dumps(
+        #     {
+        #         "size": 1,
+        #         "query": {"match": {"categories.cat_parent": id}},
+        #         "aggs": {
+        #             "categories": {"terms": {"field": "categories.cat_name.keyword"}},
+        #             "brands": {"terms": {"field": "brand.brand_name.keyword"}},
+        #             "engines": {"terms": {"field": "engines.engine_name.keyword"}},
+        #             "car_models": {"terms": {"field": "car_model.model_name.keyword"}},
+        #         },
+        #     }
+        # )
+
         r = requests.get(
             "http://localhost:9200/prod_notebook/_search",
             headers={"Content-Type": "application/json"},
             data=data,
         )
-        print(r.json())
+        response = r.json()
+
+        result = response["hits"]["hits"]
+        print(result)
 
         # request = Search(using=es, index="prod_notebook")
         # response = request.source(["id", "name"])
         # for item in response:
         #     print(item)
-        return {"id": id, "name": "Vladimir"}
+        # return json.loads(result)
+        return result
 
     elastic = String()
 
