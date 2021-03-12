968fe5b266ef05638d1529201e2b3ea464438e9f
mikohan <angara99@gmail.com>
2021-03-12 11:56:54 +0300

    request added and working

--- search_test/schema.py
+++ search_test/schema.py
@@ -3,6 +3,7 @@ from graphene_django import DjangoObjectType
 from graphene_django import DjangoListField
 from elasticsearch_dsl import Search
 from elasticsearch import Elasticsearch
+import requests, json
 
 # connections.create_connection(hosts=["localhost:9200"], timeout=20)
 es = Elasticsearch(["http://localhost:9200"])
@@ -18,11 +19,18 @@ class Query(ObjectType):
     product = Field(Product, id=Int())
 
     def resolve_product(root, info, id):
+        data = json.dumps({"query": "size": 1000, {"term": {"car_model.model_name.keyword": "HD72"}}})
+        r = requests.get(
+            "http://localhost:9200/prod_notebook/_search",
+            headers={"Content-Type": "application/json"},
+            data=data,
+        )
+        print(r.json())
 
-        request = Search(using=es, index="prod_notebook")
-        response = request.source(["id", "name"])
-        for item in response:
-            print(item)
+        # request = Search(using=es, index="prod_notebook")
+        # response = request.source(["id", "name"])
+        # for item in response:
+        #     print(item)
         return {"id": id, "name": "Vladimir"}
 
     elastic = String()
